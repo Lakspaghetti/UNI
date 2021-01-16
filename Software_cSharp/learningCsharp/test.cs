1692e434b56e9ac0d15933365f1be0d707057737
Luc <mikkelluc@gmail.com>
2021-01-16 18:30:55 +0100

    csharp

---
+++ Software_cSharp/learningCsharp/test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace learningCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 5;
+            int mySndInt = 3;
+
+            double mydouble = 4.3;
+            double mySnddouble = 3.2;
+
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(myInt + mySndInt);
+
+            Console.WriteLine(mydouble + mySnddouble);
+
+            Console.ReadLine ();
+        }
+    }
+}
