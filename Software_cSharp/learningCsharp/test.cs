f87dfdc35d0de2eff6d4a5a3df22aff3f3480c7f
Luc <mikkelluc@gmail.com>
2021-01-31 13:24:07 +0100

    forgot what to do

--- Software_cSharp/learningCsharp/test.cs
+++ Software_cSharp/learningCsharp/test.cs
@@ -6,17 +6,57 @@ namespace learningCsharp
     {
         static void Main(string[] args)
         {
+            int myTest;
             int myInt = 5;
-            int mySndInt = 3;
+            int mySndInt = 4;
 
             double mydouble = 4.3;
             double mySnddouble = 3.2;
 
+            String myString = "Mikkel";
+            String mySndString = "Luc";
+            String myThrdString = "Carlsen";
+
+            String[] myStringArray = new String[5] {"hej", "med", "dig", "bla", "bla"}; 
+
+            Array myArr = Array.CreateInstance(typeof(string), 4);
+
             Console.WriteLine("Hello World!");
+
+            myTest = 5;
+
+            Console.WriteLine(myTest);
+
+            myTest = 8;
+
+            Console.WriteLine(myTest);
 
             Console.WriteLine(myInt + mySndInt);
 
+            Console.BackgroundColor = System.ConsoleColor.Red;
+
+            Console.ForegroundColor = System.ConsoleColor.Black;
+
             Console.WriteLine(mydouble + mySnddouble);
+
+            Console.ResetColor();
+
+            Console.BackgroundColor = System.ConsoleColor.DarkBlue;
+
+            Console.ForegroundColor = System.ConsoleColor.DarkGray;
+
+            Console.WriteLine(myString + " " + mySndString + " "+ myThrdString); 
+
+            Console.ResetColor();
+
+            Console.WriteLine(myStringArray);
+
+            for (int i = 0; i < myStringArray.Length; i++)
+            {
+                Console.WriteLine(myStringArray.GetValue(i));
+            }
+
+            Console.WriteLine(myArr);
 
             Console.ReadLine ();
         }
---
+++ Software_cSharp/soloLearn/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int x = 1; x <= number; x++)
+            {  
+                string output;
+                output = (x%3==0) ? ("*") : Convert.ToString(x);
+                Console.Write(output);
+            }
+        }
+    }
+}
