773281e1195787aa953e1b987821d326f7755229
Luc <mikkelluc@gmail.com>
2021-03-15 11:13:36 +0100

    testcollatz

--- Software_cSharp/TestCollatz/CollatzTests.cs
+++ Software_cSharp/TestCollatz/CollatzTests.cs
@@ -11,5 +11,15 @@ namespace TestCollatz {
             Assert.AreEqual(CollatzImpl.Collatz(n, max_len, max_size),
                             CollatzImpl.CollatzRec(n, max_len, max_size));
         }
+
+        [TestCase(5, 3, 8)]
+        public void TestStatement(int n, int max_len, int max_size) {
+            Assert.AreEqual(CollatzImpl.Collatz(n, max_len, max_size), 1);
+        }//enters while loop, increments len, enters else part of if statement and then breaks while loop
+
+        [TestCase(2, 3, 8)]
+        public void TestBranch(int n, int max_len, int max_size) {
+            Assert.AreEqual(CollatzImpl.Collatz(n, max_len, max_size), 1);
+        } // enters if statement
     }
 }
